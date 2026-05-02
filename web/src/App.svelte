001fb6e88e87948b71bed2440a191bce47bc28fa
Tan Yin Loo <tan.yinloo@olaparty.sg>
2026-05-02 10:21:42 +0800

    fix: remove Tailwindcss.svelte, use global.css instead

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,5 +1,4 @@
 <script>
-  import Tailwindcss from "./Tailwindcss.svelte";
   import Logo from "./components/Logo.svelte";
   import Draw from "./pages/Draw.svelte";
 
@@ -10,7 +9,6 @@
 
 </style>
 
-<Tailwindcss />
 <div
   class="h-12 elevation-4 bg-red-600 flex justify-between items-center pl-6 pr-3">
   <h1 class="text-white font-bold text-lg flex">
