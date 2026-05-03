ca1a45361652e2c39b08fa4e7aad008e8e358417
tan.yinloo <tan.yinloo@olaparty.sg>
2026-05-03 12:01:42 +0800

    fix: always show empty option so positions can be unselected

--- web/src/components/GroupCard.svelte
+++ web/src/components/GroupCard.svelte
@@ -88,7 +88,7 @@
         value={group.winner.position ?? ""}
         on:change={onWinnerPositionSelect}
       >
-        {#if group.winner.position === null}<option value="">Position</option>{/if}
+        <option value="">Position</option>
         {#each winnerSelectOptions as pos}
           <option value={pos}>{pos}</option>
         {/each}
@@ -133,7 +133,7 @@
           on:change={onRunnerUpPositionSelect}
           disabled={group.winner.position === null}
         >
-          {#if group.runnerUp.position === null}<option value="">Position</option>{/if}
+          <option value="">Position</option>
           {#each runnerUpSelectOptions as pos}
             <option value={pos}>{pos}</option>
           {/each}
